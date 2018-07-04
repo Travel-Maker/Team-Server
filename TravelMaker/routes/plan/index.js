e2f2e50f2ce34103cbc27689e3822ee111649c3f
HyunJin <hj97718@naver.com>
2018-07-05 01:17:15 +0900

    add comment router and files

--- TravelMaker/routes/plan/index.js
+++ TravelMaker/routes/plan/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//전문가 : 받은 플랜 관리
+const receive = require('./receive.js');
+router.use('/receive', receive);
+
+//일정 보기
+
 
 module.exports = router;
